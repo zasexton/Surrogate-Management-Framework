3653faae889ff738e33aa0f4c154105081170029
Zack Sexton <zsexton@stanford.edu>
2022-09-02 11:29:46 -0700

    replaced externalproject with fetchcontent to build Eigen prior

--- Code/main.cpp
+++ Code/main.cpp
@@ -5,5 +5,6 @@ int main() {
   Eigen::MatrixXd m = Eigen::MatrixXd::Random(3,3);
   std::cout << m << std::endl;
   std::cout << m.inverse() << std::endl;
+  std::cout << "nothing" << std::endl;
   return 0;
 }
